a2e4d1583d7353d66b7defbca5506c0690967a96
Delamare2112 <trevor@studiolimitless.com>
2017-03-28 14:28:49 -0700

    eslinted

--- unpublishedScripts/interaction/Interaction.js
+++ unpublishedScripts/interaction/Interaction.js
@@ -10,132 +10,136 @@
 //
 
 (function(){
-	print("loading interaction script");
+    print("loading interaction script");
 
-	var Avatar = false;
-	var NPC = false;
-	var hasCenteredOnNPC = false;
-	var distance = 10;
-	var r = 8;
+    var Avatar = false;
+    var NPC = false;
+    var hasCenteredOnNPC = false;
+    var distance = 10;
+    var r = 8;
 
-	var baselineX = 0;
-	var baselineY = 0;
-	var nodRange = 20;
-	var shakeRange = 20;
+    var baselineX = 0;
+    var baselineY = 0;
+    var nodRange = 20;
+    var shakeRange = 20;
 
-	var ticker = false;
+    var ticker = false;
 
-	function callOnNPC(message) {
-		Messages.sendMessage("interactionComs", NPC + ":" + message);
-	}
+    function callOnNPC(message) {
+        Messages.sendMessage("interactionComs", NPC + ":" + message);
+    }
 
-	LimitlessSpeechRecognition.onFinishedSpeaking.connect(function(speech) {
-		print("Got: " + speech);
-		callOnNPC("voiceData:" + speech);
-	});
+    LimitlessSpeechRecognition.onFinishedSpeaking.connect(function(speech) {
+        print("Got: " + speech);
+        callOnNPC("voiceData:" + speech);
+    });
 
-	LimitlessSpeechRecognition.onReceivedTranscription.connect(function(speech) {
-		callOnNPC("speaking");
-	});
+    LimitlessSpeechRecognition.onReceivedTranscription.connect(function(speech) {
+        callOnNPC("speaking");
+    });
 
-	function setBaselineRotations(rot) {
-		baselineX = rot.x;
-		baselineY = rot.y;
-	}
+    function setBaselineRotations(rot) {
+        baselineX = rot.x;
+        baselineY = rot.y;
+    }
 
-	function findLookedAtNPC() {
-		var intersection = AvatarList.findRayIntersection({origin: MyAvatar.position, direction: Quat.getFront(Camera.getOrientation())}, true);
-		if(intersection.intersects && intersection.distance <= distance){
-			var npcAvatar = AvatarList.getAvatar(intersection.avatarID);
-			if(npcAvatar.displayName.search("NPC") != -1) {
-				setBaselineRotations(Quat.safeEulerAngles(Camera.getOrientation()));
-				return intersection.avatarID;
-			}
-		}
-		return false;
-	}
+    function findLookedAtNPC() {
+        var intersection = AvatarList.findRayIntersection({origin: MyAvatar.position, direction: Quat.getFront(Camera.getOrientation())}, true);
+        if (intersection.intersects && intersection.distance <= distance){
+            var npcAvatar = AvatarList.getAvatar(intersection.avatarID);
+            if (npcAvatar.displayName.search("NPC") != -1) {
+                setBaselineRotations(Quat.safeEulerAngles(Camera.getOrientation()));
+                return intersection.avatarID;
+            }
+        }
+        return false;
+    }
 
-	function isStillFocusedNPC() {
-		var avatar = AvatarList.getAvatar(NPC);
-		if(avatar) {
-			var avatarPosition = avatar.position;
-			return Vec3.distance(MyAvatar.position, avatarPosition) <= distance && Math.abs(Quat.dot(Camera.getOrientation(), Quat.lookAtSimple(MyAvatar.position, avatarPosition))) > 0.6;
-		}
-		return false; // NPC reference died. Maybe it crashed or we teleported to a new world?
-	}
+    function isStillFocusedNPC() {
+        var avatar = AvatarList.getAvatar(NPC);
+        if (avatar) {
+            var avatarPosition = avatar.position;
+            return Vec3.distance(MyAvatar.position, avatarPosition) <= distance && Math.abs(Quat.dot(Camera.getOrientation(), Quat.lookAtSimple(MyAvatar.position, avatarPosition))) > 0.6;
+        }
+        return false; // NPC reference died. Maybe it crashed or we teleported to a new world?
+    }
 
-	function onWeLostFocus() {
-		print("lost NPC: " + NPC);
-		callOnNPC("onLostFocused");
-		var baselineX = 0;
-		var baselineY = 0;
-		LimitlessSpeechRecognition.setListeningToVoice(false);
-	}
+    function onWeLostFocus() {
+        print("lost NPC: " + NPC);
+        callOnNPC("onLostFocused");
+        var baselineX = 0;
+        var baselineY = 0;
+        LimitlessSpeechRecognition.setListeningToVoice(false);
+    }
 
-	function onWeGainedFocus() {
-		print("found NPC: " + NPC);
-		callOnNPC("onFocused");
-		var rotation = Quat.safeEulerAngles(Camera.getOrientation());
-		baselineX = rotation.x;
-		baselineY = rotation.y;
-		LimitlessSpeechRecognition.setListeningToVoice(true);
-	}
+    function onWeGainedFocus() {
+        print("found NPC: " + NPC);
+        callOnNPC("onFocused");
+        var rotation = Quat.safeEulerAngles(Camera.getOrientation());
+        baselineX = rotation.x;
+        baselineY = rotation.y;
+        LimitlessSpeechRecognition.setListeningToVoice(true);
+    }
 
-	function checkFocus() {
-		var newNPC = findLookedAtNPC();
+    function checkFocus() {
+        var newNPC = findLookedAtNPC();
 
-		if(NPC && newNPC != NPC && !isStillFocusedNPC()) {
-			onWeLostFocus();
-			NPC = false;
-		}
-		if(!NPC && newNPC != false) {
-			NPC = newNPC;
-			onWeGainedFocus();
-		}
-	}
+        if (NPC && newNPC != NPC && !isStillFocusedNPC()) {
+            onWeLostFocus();
+            NPC = false;
+        }
+        if (!NPC && newNPC != false) {
+            NPC = newNPC;
+            onWeGainedFocus();
+        }
+    }
 
-	function checkGesture() {
-		var rotation = Quat.safeEulerAngles(Camera.getOrientation());
+    function checkGesture() {
+        var rotation = Quat.safeEulerAngles(Camera.getOrientation());
 
-		var deltaX = Math.abs(rotation.x - baselineX);
-		if(deltaX > 180)
-			deltaX -= 180;
-		var deltaY = Math.abs(rotation.y - baselineY);
-		if(deltaY > 180)
-			deltaY -= 180;
+        var deltaX = Math.abs(rotation.x - baselineX);
+        if (deltaX > 180) {
+            deltaX -= 180;
+        }
+        var deltaY = Math.abs(rotation.y - baselineY);
+        if (deltaY > 180) {
+            deltaY -= 180;
+        }
 
-		if(deltaX >= nodRange && deltaY <= shakeRange)
-			callOnNPC("onNodReceived");
-		else if (deltaY >= shakeRange && deltaX <= nodRange)
-			callOnNPC("onShakeReceived");
-	}
+        if (deltaX >= nodRange && deltaY <= shakeRange) {
+            callOnNPC("onNodReceived");
+        } else if (deltaY >= shakeRange && deltaX <= nodRange) {
+            callOnNPC("onShakeReceived");
+        }
+    }
 
-	function tick() {
-		checkFocus();
-		if(NPC)
-			checkGesture();
-	}
+    function tick() {
+        checkFocus();
+        if (NPC) {
+            checkGesture();
+        }
+    }
 
-	this.enterEntity = function(id) {
-		print("Something entered me: " + id);
-		LimitlessSpeechRecognition.setAuthKey("testKey");
-		if(!ticker) {
-			ticker = Script.setInterval(tick, 333);
-		}
-	};
-	this.leaveEntity = function(id) {
-		print("Something left me: " + id);
-		if(ticker) {
-			ticker.stop();
-			ticker = false;
-		}
-	};
-	this.unload = function() {
-		print("Okay. I'm Unloading!");
-		if(ticker) {
-			ticker.stop();
-			ticker = false;
-		}
-	}
-	print("finished loading interaction script");
-})
+    this.enterEntity = function(id) {
+        print("Something entered me: " + id);
+        LimitlessSpeechRecognition.setAuthKey("testKey");
+        if (!ticker) {
+            ticker = Script.setInterval(tick, 333);
+        }
+    };
+    this.leaveEntity = function(id) {
+        print("Something left me: " + id);
+        if (ticker) {
+            ticker.stop();
+            ticker = false;
+        }
+    };
+    this.unload = function() {
+        print("Okay. I'm Unloading!");
+        if (ticker) {
+            ticker.stop();
+            ticker = false;
+        }
+    };
+    print("finished loading interaction script");
+});
--- unpublishedScripts/interaction/NPC_AC.js
+++ unpublishedScripts/interaction/NPC_AC.js
@@ -13,16 +13,18 @@ var currentlyUsedIndices = [];
 var currentlyEngaged = false;
 var questionNumber = 0;
 function getRandomRiddle() {
-	var randIndex = null;
-	do {
-		randIndex = Math.floor(Math.random() * 15) + 1;
-	}while(randIndex in currentlyUsedIndices)
+    var randIndex = null;
+    do {
+        randIndex = Math.floor(Math.random() * 15) + 1;
+    } while (randIndex in currentlyUsedIndices);
 
-	currentlyUsedIndices.push(randIndex);
-	return randIndex.toString();
+    currentlyUsedIndices.push(randIndex);
+    return randIndex.toString();
 }
 
-Script.include("file:///home/delamare/gitclones/hifi/unpublishedScripts/interaction/NPCHelpers_v9.js", function(){print("NPCHelpers included.");main();});
+Script.include("file:///home/delamare/gitclones/hifi/unpublishedScripts/interaction/NPCHelpers_v9.js", function(){
+    print("NPCHelpers included.");main();
+});
 
 var idleAnim = "https://storage.googleapis.com/limitlessserv-144100.appspot.com/hifi%20assets/idle.fbx";
 var FST = "https://storage.googleapis.com/limitlessserv-144100.appspot.com/hifi%20assets/Animation/myYisup.fst";
@@ -40,45 +42,41 @@ Messages.subscribe("interactionComs");
 
 var gem = Entities.addEntity({position: {x: -1400.1, y: 52, z: -1280.5}, type: "Sphere", color: {red:200,green:200,blue:200}});
 function updateGem() {
-	if(audioInjector) {
-		var colorVal = (audioInjector.loudness + 0.3) * 255;
-		Entities.editEntity(gem, {color: {red:colorVal,green:0,blue:0}});
-	}
+    if (audioInjector) {
+        var colorVal = (audioInjector.loudness + 0.3) * 255;
+        Entities.editEntity(gem, {color: {red:colorVal,green:0,blue:0}});
+    }
 }
 
 function main() {
-	storyURL = "https://storage.googleapis.com/limitlessserv-144100.appspot.com/hifi%20assets/Sphinx_t8.json";
-	Messages.messageReceived.connect(function (channel, message, sender) {
-		print(sender + " -> NPC @" + Agent.sessionUUID + ": " + message);
-		if(channel === "interactionComs" && strContains(message, Agent.sessionUUID)) {
-			if(strContains(message, "onFocused")) {
-				blocked = false;
-				currentlyEngaged = true;
-				currentlyUsedIndices = [];
-				doActionFromServer("start");
-			}
-			else if (strContains(message, "onLostFocused")) {
-				blocked = false;
-				Avatar.orientation = startingOrientation;
-				npcContinueStory('', 'https://storage.googleapis.com/limitlessserv-144100.appspot.com/hifi%20assets/beingOfLightWave.fbx', 'start');
-			}
-			else if (strContains(message, "speaking")) {
-				// resetIgnoreTimer();
-			}
-			else {
-				var voiceDataIndex = message.search("voiceData");
-				if (voiceDataIndex != -1) {
-					var words = message.substr(voiceDataIndex+10);
-					if(strContains(words, "repeat") || (strContains(words, "say") && strContains(words, "again"))) {
-						doActionFromServer("init");
-					} 
-					else {
-						doActionFromServer("words", words);
-					}
-				}
-			}
-		}
-	});
-	Script.update.connect(updateGem);
-	playAnim(idleAnim, true);
+    storyURL = "https://storage.googleapis.com/limitlessserv-144100.appspot.com/hifi%20assets/Sphinx_t8.json";
+    Messages.messageReceived.connect(function (channel, message, sender) {
+        print(sender + " -> NPC @" + Agent.sessionUUID + ": " + message);
+        if (channel === "interactionComs" && strContains(message, Agent.sessionUUID)) {
+            if (strContains(message, "onFocused")) {
+                blocked = false;
+                currentlyEngaged = true;
+                currentlyUsedIndices = [];
+                doActionFromServer("start");
+            } else if (strContains(message, "onLostFocused")) {
+                blocked = false;
+                Avatar.orientation = startingOrientation;
+                npcContinueStory('', 'https://storage.googleapis.com/limitlessserv-144100.appspot.com/hifi%20assets/beingOfLightWave.fbx', 'start');
+            } else if (strContains(message, "speaking")) {
+                // resetIgnoreTimer();
+            } else {
+                var voiceDataIndex = message.search("voiceData");
+                if (voiceDataIndex != -1) {
+                    var words = message.substr(voiceDataIndex+10);
+                    if (strContains(words, "repeat") || (strContains(words, "say") && strContains(words, "again"))) {
+                        doActionFromServer("init");
+                    } else {
+                        doActionFromServer("words", words);
+                    }
+                }
+            }
+        }
+    });
+    Script.update.connect(updateGem);
+    playAnim(idleAnim, true);
 }
